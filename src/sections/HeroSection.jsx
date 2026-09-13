22d6d3c2c66dd51e4324eda9ccfca04674516cf5
Jelena <jelena33.bliss@gmail.com>
2026-09-13 13:42:01 +0200

    src

---
+++ src/sections/HeroSection.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const HeroSection = () => {
+  return (
+   <section className="flex flex-col items-center min-h-screen justify-center bg-[url('/assets/heroBanner.png')] bg-cover bg-center bg-no-repeat px-4 pt-20">
+
+   </section>
+
+  )
+}
+
+export default HeroSection

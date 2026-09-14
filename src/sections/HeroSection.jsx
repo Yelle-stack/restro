c918e54f5b7226d23180fecead74bb5084b6268f
Jelena <jelena33.bliss@gmail.com>
2026-09-14 10:18:54 +0200

    src

--- src/sections/HeroSection.jsx
+++ src/sections/HeroSection.jsx
@@ -1,8 +1,12 @@
-import React from 'react'
+import Animated from "../components/Animated"
 
 const HeroSection = () => {
   return (
-   <section className="flex flex-col items-center min-h-screen justify-center bg-[url('/assets/heroBanner.png')] bg-cover bg-center bg-no-repeat px-4 pt-20">
+   <section className="flex flex-col items-center min-h-screen justify-start bg-[url('/assets/heroBanner.png')] bg-cover bg-center bg-no-repeat px-4 pt-20">
+
+    <Animated y={-20} delay={0.2}>
+        <p className='text-orange-600'>WHERE FLAVOR MEETS ELEGANCE</p>
+    </Animated>
 
    </section>
 

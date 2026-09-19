abf291240671bad022d2958982d73c54395540b5
Jelena <jelena33.bliss@gmail.com>
2026-09-19 17:26:02 +0200

    srcAbout

--- src/sections/About.jsx
+++ src/sections/About.jsx
@@ -20,13 +20,25 @@ const About = () => {
            <img src="/assets/iconR.png" alt="iconRight" />
         </Animated>
 
-        <Animated scale={0.8} y={0} className="flex items-center gap-2">
-           
+        <Animated>
            <h2 className='mt-5 text-4xl md:text-5xl text-balance'>
             Experience dining beyond expectations
            </h2>
-           
         </Animated>
+
+        <Animated delay={0.2}>
+           <p className='mt-4.5 text-zinc-600 max-w-sm'>We combine fresh local ingredients, creative recipes and elegant presentation to deliver a memorable experience with every visit.</p>
+        </Animated>
+
+        <Animated className="mt-9 bg-orange-500 text-white p-2 pr-8 rounded-lg flex items-center gap-3 w-fit">
+          <img src="/assets/about.png" alt="Bistro Royale Location Preview" 
+          className='size-15 rounded-lg object-cover shrink-0'/>
+          <div className='flex flex-col gap-2'>
+            <p className='font-medium'>Bistro Royale, Paris</p>
+            <a href="#">View on Map</a>
+          </div>
+        </Animated>
+
        </div>
 
 

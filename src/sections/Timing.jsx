56b8265dd9b2c7868d7f2bb7711d3d840f407884
Jelena <jelena33.bliss@gmail.com>
2026-09-25 15:56:45 +0200

    srcAppTiming

---
+++ src/sections/Timing.jsx
@@ -0,0 +1,14 @@
+import Animated from '../components/Animated'
+
+const Timing = () => {
+  return (
+    <section id='timing' className='px-auto mt-44'>
+      <Animated scale={0.8} y={0}
+      className="w-full max-w-5xl h-162.5 rounded-3xl bg-cover bg-center flex items-center justify-center md:justify-start px-6 md:px-14 overflow-hidden mx-auto bg-[url('/assets/restro-timing.png')]">
+        
+      </Animated>
+    </section>
+  )
+}
+
+export default Timing
